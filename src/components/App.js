c3cd9a67e4934f195e22554cb84094f02d6ff36d
raafa16 <raafa16@hotmail.com>
2019-02-11 16:32:46 -0500

    async await syntax

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,17 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID 2438acd53e0ea878e5be294dfe31a4672bb3df6496ec36ef598551c9af27e60e"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
-  };
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 2438acd53e0ea878e5be294dfe31a4672bb3df6496ec36ef598551c9af27e60e"
+      }
+    });
+    console.log(response.data.results);
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
