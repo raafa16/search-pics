8c36ab8bf9957e48d9e35e18b44b8ace123400db
raafa16 <raafa16@hotmail.com>
2019-02-11 16:44:40 -0500

    async await refactor to fix this context

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,13 +12,15 @@ class App extends React.Component {
           "Client-ID 2438acd53e0ea878e5be294dfe31a4672bb3df6496ec36ef598551c9af27e60e"
       }
     });
-    console.log(response.data.results);
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
