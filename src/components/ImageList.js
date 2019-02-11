843eabc4c447b3f2f4950e8b369d0ec6eeda776d
raafa16 <raafa16@hotmail.com>
2019-02-11 17:47:47 -0500

    v1 implementation done

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-const ImageList = () => {
-  return <div>ImageList</div>;
+const ImageList = props => {
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.regular} alt={description} />;
+  });
+  return <div>{images}</div>;
 };
 
 export default ImageList;
