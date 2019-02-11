ebafe29a6d813428a86a88b96bc2b80a26e3bc9e
raafa16 <raafa16@hotmail.com>
2019-02-11 16:30:54 -0500

    unsplash api and async await

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit = term => {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 2438acd53e0ea878e5be294dfe31a4672bb3df6496ec36ef598551c9af27e60e"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
